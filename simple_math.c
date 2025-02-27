c2774f1bfe23f81dd77b483d06e70d33393980e5
Alessio Mallamaci <alessiomallamaci@gmail.com>
2025-02-27 14:08:33 +0100

    new version of simple math with max - no test

--- simple_math.c
+++ simple_math.c
@@ -1,15 +1,22 @@
-/**
- * \file simple_math.h
- * \brief a toy example of math library
- * \author Gabriele Triglia
+/** \file simple_math.h
+    \brief a toy example of math library
+    \author Alessio Mallamaci
  */
+
 #include "simple_math.h"
 
 /**
- * \param a primo numero
- * \param b secondo numero
- * \return media 
- */
-float mean(int a, int b){
+   \param a primo numero
+   \param b secondo numero
+   \return media 
+*/
+
+float mean(int a, int b) {
     return (a + b)/2;
+}
+
+int max (int a, int b) {
+    if (a > b)
+        return a;
+    return b;
 }
--- simple_math.h
+++ simple_math.h
@@ -1,12 +1,14 @@
 /**
-* \file simple_math.h
-* \brief a toy example of math library
-* \author Gabriele Triglia
+  \file simple_math.h
+  \brief a toy example of math library
+  \author Alessio Mallamaci
 */
 
 #ifndef _SIMPLEMATH_
-    #define _SIMPLEMATH_
-    float mean (int a, int b);
-#endif 
+#define _SIMPLEMATH_
 
+float mean (int a, int b); // fa la media tra due numeri
 
+int max (int a, int b); //trova il massimo tra due parametri
+
+#endif
