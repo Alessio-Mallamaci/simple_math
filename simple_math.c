d9c5dcea8e87cb668252fc183e1df67619846e35
Alessio Mallamaci <alessiomallamaci@gmail.com>
2025-02-27 22:08:55 +0100

    codice commentato, maca ancora la parte del main

--- simple_math.c
+++ simple_math.c
@@ -1,25 +1,28 @@
 /** \file simple_math.h
     \brief a toy example of math library
     \author Alessio Mallamaci
- */
+*/
 
 
 #include "simple_math.h"
 
-cx_float_t cx_mean (cx_int_t a, cx_int_t b){
-    
-    cx_float_t average;
+//funzione che calcola la media di due numeri immaginari
+cx_float_t cx_mean (cx_int_t a, cx_int_t b) {
 
-    average.re=(a.re + b.re)/2;
-    average.imm=(a.imm + b.imm)/2;
+    cx_float_t average; //variabile float per salvare il valore della media
+
+    average.re = (a.re + b.re)/2;
+    average.imm = (a.imm + b.imm)/2;
 
     return average;
 }
 
+//funzione MEAN
 float mean(int a, int b) {
     return (a + b)/2;
 }
 
+//funzione MAX 
 int max (int a, int b) {
     if (a > b)
         return a;
--- simple_math.h
+++ simple_math.h
@@ -7,19 +7,23 @@
 #ifndef _SIMPLEMATH_
 #define _SIMPLEMATH_
 
+//struct per definire una variabile complessa formata da valori int:
 typedef struct complex_int {
   int re;
   int imm;
 } cx_int_t;
 
+//struct per definire una variabile complessa formata da valori float:
 typedef struct complex_float {
   float re;
   float imm;
 } cx_float_t;
 
-float mean (int a, int b); // fa la media tra due numeri
-cx_float_t cx_mean (cx_int_t a, cx_int_t b);
+//funzione che fa la MEDIA di due valori:
+float mean (int a, int b); // fa la media tra due numeri int
+cx_float_t cx_mean (cx_int_t a, cx_int_t b); // fa la media di due numeri float
 
-int max (int a, int b); //trova il massimo tra due parametri
+//funzione che trova il valore MAX di due valori:
+int max (int a, int b); // trova il massimo tra due parametri
 
 #endif
