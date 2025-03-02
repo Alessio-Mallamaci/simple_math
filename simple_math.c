516c50a43c4994bf3e3053c3fd3d6f8a4bad3749
Alessio Mallamaci <alessiomallamaci@gmail.com>
2025-03-02 11:36:54 +0100

    alcune modifiche

--- simple_math.c
+++ simple_math.c
@@ -1,4 +1,5 @@
-/** \file simple_math.h
+/** 
+    \file simple_math.h
     \brief a toy example of math library
     \author Alessio Mallamaci
 */
@@ -9,7 +10,7 @@
 //funzione che calcola la media di due numeri immaginari
 cx_float_t cx_mean (cx_int_t a, cx_int_t b) {
 
-    cx_float_t average; //variabile float per salvare il valore della media
+    cx_float_t average; //struct float per salvare il valore della media
 
     average.re = (a.re + b.re)/2;
     average.imm = (a.imm + b.imm)/2;
@@ -17,12 +18,12 @@ cx_float_t cx_mean (cx_int_t a, cx_int_t b) {
     return average;
 }
 
-//funzione MEAN
+// Funzione MEAN
 float mean(int a, int b) {
     return (a + b)/2;
 }
 
-//funzione MAX 
+// Funzione MAX 
 int max (int a, int b) {
     if (a > b)
         return a;
